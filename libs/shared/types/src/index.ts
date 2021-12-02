2b17b8ac391c092aa48a42fa16ef334b01902822
Mario Andres <marioarendon2@gmail.com>
2021-12-02 00:20:59 -0600

    feat(backend): add category schema

--- libs/shared/types/src/index.ts
+++ libs/shared/types/src/index.ts
@@ -21,55 +21,96 @@ export interface User_WhereInput {
     id?: Nullable<string>;
     email?: Nullable<string>;
     username?: Nullable<string>;
-    password?: Nullable<string>;
     type?: Nullable<string>;
     active?: Nullable<string>;
+    password?: Nullable<string>;
 }
 
 export interface User_OrderByInput {
     id?: Nullable<OrderBy>;
     email?: Nullable<OrderBy>;
     username?: Nullable<OrderBy>;
-    password?: Nullable<OrderBy>;
     type?: Nullable<OrderBy>;
     active?: Nullable<OrderBy>;
+    password?: Nullable<OrderBy>;
+}
+
+export interface Category_WhereInput {
+    id?: Nullable<string>;
+    name?: Nullable<string>;
+    system_defined?: Nullable<string>;
+    user_id?: Nullable<string>;
+}
+
+export interface Category_OrderByInput {
+    id?: Nullable<OrderBy>;
+    name?: Nullable<OrderBy>;
+    system_defined?: Nullable<OrderBy>;
+    user_id?: Nullable<OrderBy>;
+}
+
+export interface Category_InsertInput {
+    id: number;
+    name: string;
+    system_defined: number;
+    user_id: number;
+}
+
+export interface Category_UpdateInput {
+    id?: Nullable<number>;
+    name?: Nullable<string>;
+    system_defined?: Nullable<number>;
+    user_id?: Nullable<number>;
 }
 
 export interface User_InsertInput {
     id: number;
     email: string;
     username?: Nullable<string>;
-    password?: Nullable<string>;
     type?: Nullable<User_type>;
     active: number;
+    password?: Nullable<string>;
 }
 
 export interface User_UpdateInput {
     id?: Nullable<number>;
     email?: Nullable<string>;
     username?: Nullable<string>;
-    password?: Nullable<string>;
     type?: Nullable<User_type>;
     active?: Nullable<number>;
+    password?: Nullable<string>;
 }
 
 export interface IQuery {
+    Category(limit?: Nullable<number>, offset?: Nullable<number>, where?: Nullable<Category_WhereInput>, orderBy?: Nullable<Category_OrderByInput>): Nullable<Nullable<Category>[]> | Promise<Nullable<Nullable<Category>[]>>;
+    count_Category(where?: Nullable<Category_WhereInput>): Nullable<number> | Promise<Nullable<number>>;
     User(limit?: Nullable<number>, offset?: Nullable<number>, where?: Nullable<User_WhereInput>, orderBy?: Nullable<User_OrderByInput>): Nullable<Nullable<User>[]> | Promise<Nullable<Nullable<User>[]>>;
     count_User(where?: Nullable<User_WhereInput>): Nullable<number> | Promise<Nullable<number>>;
     login(user: User_WhereInput): Nullable<User> | Promise<Nullable<User>>;
     me(): Nullable<User> | Promise<Nullable<User>>;
 }
 
+export interface Category {
+    id: number;
+    name: string;
+    system_defined: number;
+    user_id: number;
+    User?: Nullable<Nullable<User>[]>;
+}
+
 export interface User {
     id: number;
     email: string;
     username?: Nullable<string>;
-    password?: Nullable<string>;
     type?: Nullable<User_type>;
     active: number;
+    password?: Nullable<string>;
 }
 
 export interface IMutation {
+    insert_Category(Category: Category_InsertInput): Nullable<Category> | Promise<Nullable<Category>>;
+    update_Category(Category: Category_UpdateInput, where?: Nullable<Category_WhereInput>): Nullable<Category> | Promise<Nullable<Category>>;
+    delete_Category(where?: Nullable<Category_WhereInput>): Nullable<boolean> | Promise<Nullable<boolean>>;
     insert_User(User: User_InsertInput): Nullable<User> | Promise<Nullable<User>>;
     update_User(User: User_UpdateInput, where?: Nullable<User_WhereInput>): Nullable<User> | Promise<Nullable<User>>;
     delete_User(where?: Nullable<User_WhereInput>): Nullable<boolean> | Promise<Nullable<boolean>>;
