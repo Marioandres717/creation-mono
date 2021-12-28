448c42fe55c7f3dd698e87994a2425a697e245bd
Mario Andres <marioarendon2@gmail.com>
2021-12-28 16:59:37 -0600

    feat(backend): add timeout interceptor

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app/app.module';
 import { AllExceptionFilter } from './app/filters';
+import { TimeoutInterceptor } from './app/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,11 +21,13 @@ async function bootstrap() {
       skipMissingProperties: true,
     })
   );
+  app.useGlobalInterceptors(new TimeoutInterceptor());
   if (process.env.NODE_ENV === 'production') {
     app.useGlobalFilters(new AllExceptionFilter());
   }
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
   Logger.log(
     `🚀 Graphql playground is running on: http://localhost:${port}/graphql`
