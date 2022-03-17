d3c25a05fb2b500a932ef9fd853e2656a1260dba
Sebastian <sebas.cj@gmail.com>
2022-03-17 18:05:54 -0500

    feat(frontend): tab component

---
+++ apps/frontend/src/app/components/TabsComponent/TabsComponent.tsx
@@ -0,0 +1,83 @@
+import { ReactElement, useCallback, useState } from 'react';
+import styles from './TabsComponent.module.css';
+
+type Props = {
+  title: string;
+  index: number;
+  setSelectedTab: (index: number) => void;
+  selectedTab: number;
+};
+type Props2 = {
+  children: ReactElement[];
+};
+type Props3 = {
+  title: string;
+};
+
+const TabComponent = () => {
+  return (
+    <Tabs>
+      <Tab title="tab1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam
+      </Tab>
+      <Tab title="tab2">
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit
+      </Tab>
+      <Tab title="tab 3">
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </Tab>
+    </Tabs>
+  );
+};
+
+export default TabComponent;
+
+const Tab: React.FC<Props3> = ({ children }) => {
+  return <div className={styles['tab-panel']}>{children}</div>;
+};
+
+const Tabtitle: React.FC<Props> = ({
+  title,
+  setSelectedTab,
+  index,
+  selectedTab,
+}) => {
+  const onClick = useCallback(() => {
+    setSelectedTab(index);
+  }, [setSelectedTab, index]);
+  return (
+    <div className={styles['tab-item']}>
+      <button
+        className={index === selectedTab ? styles['tabs-active'] : styles.tabs}
+        onClick={onClick}
+      >
+        {title}
+      </button>
+    </div>
+  );
+};
+
+const Tabs: React.FC<Props2> = ({ children }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  return (
+    <div className={styles.container}>
+      <ul className={styles['tabs-container']}>
+        {children.map((item, index) => (
+          <Tabtitle
+            key={index}
+            title={item.props.title}
+            index={index}
+            setSelectedTab={setSelectedTab}
+            selectedTab={selectedTab}
+          />
+        ))}
+      </ul>
+      {children[selectedTab]}
+    </div>
+  );
+};
+
+export { Tab, Tabtitle };
--- apps/frontend/src/app/pages/profile/profile.tsx
+++ apps/frontend/src/app/pages/profile/profile.tsx
@@ -1,15 +1,9 @@
 import Layout from '../../components/layout/layout';
-import styles from './profile.module.css';
+import TabsComponent from '../../components/TabsComponent/TabsComponent';
 const Profile = () => {
   return (
-    <Layout className={styles.main}>
-      <div className={styles.box1}>Profile Page</div>
-      <div className={styles.box2}>Profile Page</div>
-      <div className={styles.box3}>
-        <div className={styles.boxItem1}>Profile Page</div>
-        <div className={styles.boxItem2}>Profile Page</div>
-        <div className={styles.boxItem3}>Profile Page</div>
-      </div>
+    <Layout>
+      <TabsComponent />
     </Layout>
   );
 };
